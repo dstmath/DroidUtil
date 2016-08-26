3369d98caa328bc482e622912a572593601f5d91
dstmath <dstmath@163.com>
2016-08-26 17:38:07 +0800

    init

--- lib/IORedirect/build/intermediates/ndk/release/obj/local/armeabi/objs/iohook/home/toor/code/DroidUtil/lib/IORedirect/src/main/jni/MSHook/x86_64.o.d
+++
@@ -1,8 +0,0 @@
-/home/toor/code/DroidUtil/lib/IORedirect/build/intermediates/ndk/release/obj/local/armeabi/objs/iohook//home/toor/code/DroidUtil/lib/IORedirect/src/main/jni/MSHook/x86_64.o: \
- /home/toor/code/DroidUtil/lib/IORedirect/src/main/jni/MSHook/x86_64.cpp \
- /home/toor/android/android-sdk-linux/ndk-bundle/sources/cxx-stl/stlport/stlport/string.h \
- /home/toor/code/DroidUtil/lib/IORedirect/src/main/jni/MSHook/x86_64.h
-
-/home/toor/android/android-sdk-linux/ndk-bundle/sources/cxx-stl/stlport/stlport/string.h:
-
-/home/toor/code/DroidUtil/lib/IORedirect/src/main/jni/MSHook/x86_64.h:
